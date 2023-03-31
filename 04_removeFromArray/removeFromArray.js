d3363d47f48c004da0e366edb7bc8874ac5c9a30
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-03-31 13:58:51 +0800

    Solved test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,17 @@
-const removeFromArray = function(arrayElements, itemToRemove) {
+const removeFromArray = function(arrayElements, itemToRemove, meow) {
+    
+    let items = [itemToRemove, meow];
 
-    for (let a = 0; a = itemToRemove.length(); a++){
-    let location = arrayElements.indexOf(itemToRemove);
-    arrayElements.splice(location, 1)
+    for (let a = 0; a < items.length; a++){
+        let location = arrayElements.indexOf(items[a]);
+        arrayElements.splice(location, 1)
     }
+    
     return arrayElements;
 
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+// console.log(meow);
 // Do not edit below this line
 module.exports = removeFromArray;
