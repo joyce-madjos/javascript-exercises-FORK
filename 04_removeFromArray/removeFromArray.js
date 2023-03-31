d2bf0b32e856f781779f6879e92622e9bdf2e7dd
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-03-31 13:17:22 +0800

    Added for loop to remove multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
 const removeFromArray = function(arrayElements, itemToRemove) {
 
+    for (let a = 0; a = itemToRemove.length(); a++){
     let location = arrayElements.indexOf(itemToRemove);
     arrayElements.splice(location, 1)
+    }
     return arrayElements;
 
 };
